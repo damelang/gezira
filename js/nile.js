9390c8f10ab2fc07cc2d412747a02e6ec0ad77fa
Dan Amelang <dan@amelang.net>
2009-09-16 21:36:58 -0700

    Fix bugs in GroupBy and Interleave, rename Echo to Id, add 'combine' and Mix.

--- js/nile.js
+++ js/nile.js
@@ -1,30 +1,59 @@
 var nile = {};
 
+nile.debug = function(s) {
+  return;
+  document.body.appendChild(document.createElement('div')).innerHTML = s;
+};
+
 nile.pipeline = function() {
   var k = arguments[arguments.length - 1];
-  for (var i = arguments.length - 2; i >= 0; i--) {
+  for (var i = arguments.length - 2; i >= 0; i--)
     k = arguments[i](k);
-  }
   return k;
 };
 
-nile.Echo = function(downstream) {
+nile.combine = function() {
+  var ks = Array.prototype.slice.call(arguments);
+  return function(downstream) {
+    return nile.pipeline.apply(null, ks.concat(downstream))
+  };
+};
+
+nile.Id = function(downstream) {
   return function(input) {
     downstream(input);
   };
 };
 
-nile.Interleave2 = function(k1, k2) {
+nile.Interleave = function(k1, size1, k2, size2) {
   return function(downstream) {
     return function(input) {
+      var output = [];
       var out1;
       var out2;
       k1(function(input) { out1 = input })(input.slice(0));
       k2(function(input) { out2 = input })(input);
-      var output = [];
-      for (var i = 0; i < out1.length; i++)
-        output.push(out1[i], out2[i]);
+      var outputsize = out1.length / size1;
+      for (var i = 0; i < outputsize; i++) {
+        for (var j = 0; j < size1; j++)
+          output.push(out1[i * size1 + j]);
+        for (var j = 0; j < size2; j++)
+          output.push(out2[i * size2 + j]);
+      }
       downstream(output);
+    };
+  };
+};
+
+nile.Mix = function(k1, k2, k3, k4) {
+  return function(downstream) {
+    return function(input) {
+      var o1, o2, o3, o4;
+      k1(function(input) { o1 = input })(input.slice(0));
+      k2(function(input) { o2 = input })(input.slice(0));
+      k3(function(input) { o3 = input })(input.slice(0));
+      k4(function(input) { o4 = input })(input.slice(0));
+      downstream(o1.concat(o2).concat(o3).concat(o4));
     };
   };
 };
@@ -54,7 +83,7 @@ nile.SortBy = function(index, size) {
     return function(input) {
       var vectors = [];
       for (var i = 0; i < input.length; i += size)
-        vectors.push(input.slice(i, size));
+        vectors.push(input.slice(i, i + size));
       vectors.sort(function(a, b) {
         if (a[index] < b[index])
           return -1;
