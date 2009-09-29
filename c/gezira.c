92e7a21af6f44f3f158202343e2498d1034b30b1
Dan Amelang <dan@amelang.net>
2009-09-28 21:19:31 -0700

    Define "real" in gezira.c to eliminate some noise.

--- c/gezira.c
+++ c/gezira.c
@@ -1,8 +1,10 @@
 #include "gezira.h"
 
+#define real nile_Real_t
+
 nile_Kernel_t *
 gezira_Sampler (gezira_Sampler_t *k,
-                nile_Real_t start_x, nile_Real_t start_y)
+                real start_x, real start_y)
 {
     k->start_x = start_x;
     k->start_y = start_y;
@@ -12,12 +14,12 @@ gezira_Sampler (gezira_Sampler_t *k,
 static void
 UniformColor_process (nile_Kernel_t *k, nile_Buffer_t *in, nile_Buffer_t *out)
 {
+    /* TODO */
 }
 
 gezira_Sampler_t *
 gezira_UniformColor (gezira_UniformColor_t *k,
-                     nile_Real_t c_a, nile_Real_t c_r,
-                     nile_Real_t c_g, nile_Real_t c_b)
+                     real c_a, real c_r, real c_g, real c_b)
 {
     k->sampler.kernel.process = UniformColor_process;
     k->c_a = c_a;
