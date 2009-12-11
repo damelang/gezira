b5eb001802bd9e6739d89b89b9f6790ffe544ec8
Dan Amelang <dan@amelang.net>
2009-12-11 02:45:43 -0800

    SDL integration complete.
    Sample app still hangs, though.

--- c/main.c
+++ c/main.c
@@ -1,12 +1,60 @@
 #include <stdio.h>
-#include "gezira.h"
+#include "gezira-sdl.h"
+
+#define NTHREADS 4
+#define DEFAULT_WIDTH  500
+#define DEFAULT_HEIGHT 500
+
+#define DIE(s, ...) \
+do { \
+    fprintf (stderr, "(%s:%4d) " s "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+    exit (1); \
+} while (0)
+
+nile_Real_t path[] =
+{
+    100, 100, 300, 200,
+    300, 200, 200, 300,
+    200, 300, 100, 100
+};
+int path_n = sizeof (path) / sizeof (path[0]);
 
 int
-main ()
+main (int argc, char **argv)
 {
-    char memory[100000];
-    nile_t *nl = nile_begin (4, memory, sizeof (memory));
+    SDL_Surface *image;
+    nile_t *nl;
+    char mem[1000000];
+
+    if (SDL_Init (SDL_INIT_VIDEO | SDL_INIT_TIMER) == -1)
+        DIE ("SDL_Init failed: %s", SDL_GetError ());
+    if (!SDL_SetVideoMode (DEFAULT_WIDTH, DEFAULT_HEIGHT, 0,
+                           SDL_HWSURFACE | SDL_ANYFORMAT))
+        DIE ("SDL_SetVideoMode failed: %s", SDL_GetError ());
+    image = SDL_GetVideoSurface ();
+
+    nl = nile_begin (NTHREADS, mem, sizeof (mem));
+
+    SDL_LockSurface (image);
+
+        nile_Kernel_t *pipeline = nile_Pipeline (nl,
+            gezira_DecomposeBezier (nl),
+            gezira_Render (nl,
+                gezira_UniformColor (nl, 1, 1, 0, 0),
+                gezira_SDL_WriteImage (nl, image)),
+                NULL);
+
+        nile_feed (nl, pipeline, path, path_n, 1);
+        nile_sync (nl);
+
+    SDL_UnlockSurface (image);
+    SDL_UpdateRect (image, 0, 0, 0, 0);
+
+    SDL_Delay (10000);
+
     nile_end (nl);
+    SDL_Quit ();
     printf ("done\n");
+
     return 0;
 }
