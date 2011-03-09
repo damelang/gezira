731ad62418baa4244fd34d6fce92ad98aa2e66fb
Dan Amelang <dan@amelang.net>
2011-03-08 22:47:38 -0800

    Fix ARG32 -> ARGB32 typo

--- c/gezira-image.h
+++ c/gezira-image.h
@@ -13,8 +13,8 @@ gezira_WriteToImage_ARGB32 (nile_Process_t *p, uint32_t *pixels,
                             int width, int height, int stride);
 
 nile_Process_t *
-gezira_WriteToImage_ARG32_UniformColor (nile_Process_t *p, uint32_t *pixels,
-                                        int width, int height, int stride,
-                                        float a, float r, float g, float b);
+gezira_WriteToImage_ARGB32_UniformColor (nile_Process_t *p, uint32_t *pixels,
+                                         int width, int height, int stride,
+                                         float a, float r, float g, float b);
 
 #endif
