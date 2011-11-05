fafc1674ea4792b485c1216206bfa2e11eb76b20
Dan Amelang <dan@amelang.net>
2011-11-05 01:27:04 -0700

    Update WriteToImage and Composite...OverImage and the generated code to match new rasterizer output.
    Also, the WriteToImage and Composite...OverImage now do bounds checking on their
    input, so we don't write outside of the bitmap memory. It should have been this way
    from the beginning (sorry Bert).

--- c/gezira-image.c
+++ c/gezira-image.c
@@ -80,8 +80,8 @@ static nile_Buffer_t *
 gezira_WriteToImage_ARGB32_body (nile_Process_t *p, nile_Buffer_t *in, nile_Buffer_t *out)
 {
     gezira_Image_ARGB32_t image = *(gezira_Image_ARGB32_t *) nile_Process_vars (p);
-    Real width = nile_Real (image.width);
-    Real height = nile_Real (image.height);
+    int width  = image.width;
+    int height = image.height;
 
     int m = (in->tail - in->head) / 8;
     while (m--) {
@@ -95,7 +95,9 @@ gezira_WriteToImage_ARGB32_body (nile_Process_t *p, nile_Buffer_t *in, nile_Buff
         uint8_t ic = Real_to_uint8_t (nile_Buffer_pop_head (in));
         uint32_t *px = &image.pixels[x + y * image.stride];
 
-        if (!c)
+        if (c == 0 ||
+            x <  0 || width  <= x ||
+            y <  0 || height <= y)
             continue;
 
         uint32_t d = *px;
@@ -166,70 +168,77 @@ gezira_CompositeUniformColorOverImage_ARGB32_body (nile_Process_t *p, nile_Buffe
         int     l = nile_Real_toi (nile_Buffer_pop_head (in));
         uint32_t *px = &v.image.pixels[x + y * v.image.stride];
 
-        if (c) { // required, not just an optimization!
-            uint32_t d  = *px;
-            uint8_t  da = d >> 24;
-            uint8_t  dr = d >> 16;
-            uint8_t  dg = d >>  8;
-            uint8_t  db = d >>  0;
-            uint16_t a  = v.a8 * c;
-            uint16_t r  = v.r8 * c;
-            uint16_t g  = v.g8 * c;
-            uint16_t b  = v.b8 * c;
+        if (c == 0 || l <= 0                  ||
+            x <  0 || v.image.width  <  x + l ||
+            y <  0 || v.image.height <= y)
+            continue;
 
-            uint16_t ia = (255 * 255 - a) >> 8;
-            a = (a + da * ia) >> 8;
-            r = (r + dr * ia) >> 8;
-            g = (g + dg * ia) >> 8;
-            b = (b + db * ia) >> 8;
+        if (c == 255) {
+            if (v.ia8 == 0) {
+                while (l--)
+                    *px++ = v.a8r8g8b8;
+            }
+            else {
+                int32_t *px_ = (int32_t *) px;
+                while (l--) {
+                    int32_t  d  = *px_;
+                    uint8_t  da = d >> 24;
+                    uint8_t  dr = d >> 16;
+                    uint8_t  dg = d >>  8;
+                    uint8_t  db = d >>  0;
+                    uint16_t a  = v.a16 + da * v.ia8;
+                    uint16_t r  = v.r16 + dr * v.ia8;
+                    uint16_t g  = v.g16 + dg * v.ia8;
+                    uint16_t b  = v.b16 + db * v.ia8;
 
-            /*
-            uint16_t ia = (255 * 255 - a) + 128;
-            ia = (ia + (ia >> 8)) >> 8;
-            a = a + da * ia + 128;
-            r = r + dr * ia + 128;
-            g = g + dg * ia + 128;
-            b = b + db * ia + 128;
-            a = (a + (a >> 8)) >> 8;
-            r = (r + (r >> 8)) >> 8;
-            g = (g + (g >> 8)) >> 8;
-            b = (b + (b >> 8)) >> 8;
-            */
+                    a >>= 8;
+                    r >>= 8;
+                    g >>= 8;
+                    b >>= 8;
 
-            *px = (a << 24) | (r << 16) | (g << 8) | (b << 0);
-        }
-        px++;
+                    /*
+                    a = (a + (a >> 8)) >> 8;
+                    r = (r + (r >> 8)) >> 8;
+                    g = (g + (g >> 8)) >> 8;
+                    b = (b + (b >> 8)) >> 8;
+                    */
 
-        if (!v.ia8) {
-            while (l--)
-                *px++ = v.a8r8g8b8;
+                    *px_++ = (a << 24) | (r << 16) | (g << 8) | (b << 0);
+                }
+            }
         }
         else {
-            int32_t *px_ = (int32_t *) px;
             while (l--) {
-                int32_t d  = *px_;
+                uint32_t d  = *px;
                 uint8_t  da = d >> 24;
                 uint8_t  dr = d >> 16;
                 uint8_t  dg = d >>  8;
                 uint8_t  db = d >>  0;
-                uint16_t a  = v.a16 + da * v.ia8;
-                uint16_t r  = v.r16 + dr * v.ia8;
-                uint16_t g  = v.g16 + dg * v.ia8;
-                uint16_t b  = v.b16 + db * v.ia8;
+                uint16_t a  = v.a8 * c;
+                uint16_t r  = v.r8 * c;
+                uint16_t g  = v.g8 * c;
+                uint16_t b  = v.b8 * c;
 
-                a >>= 8;
-                r >>= 8;
-                g >>= 8;
-                b >>= 8;
+                uint16_t ia = (255 * 255 - a) >> 8;
+                a = (a + da * ia) >> 8;
+                r = (r + dr * ia) >> 8;
+                g = (g + dg * ia) >> 8;
+                b = (b + db * ia) >> 8;
 
                 /*
+                uint16_t ia = (255 * 255 - a) + 128;
+                ia = (ia + (ia >> 8)) >> 8;
+                a = a + da * ia + 128;
+                r = r + dr * ia + 128;
+                g = g + dg * ia + 128;
+                b = b + db * ia + 128;
                 a = (a + (a >> 8)) >> 8;
                 r = (r + (r >> 8)) >> 8;
                 g = (g + (g >> 8)) >> 8;
                 b = (b + (b >> 8)) >> 8;
                 */
 
-                *px_++ = (a << 24) | (r << 16) | (g << 8) | (b << 0);
+                *px++ = (a << 24) | (r << 16) | (g << 8) | (b << 0);
             }
         }
     }
